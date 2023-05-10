5f09fdbd993927f53ca0d072aec533d954133b27
Zed Amaar <mcverdict@gmail.com>
2023-05-10 22:55:40 +0100

    added movement protection

--- index.js
+++ index.js
@@ -47,21 +47,29 @@ document.body.addEventListener('keydown', keyDown);
 function keyDown(event){
     //Up arrow
     if(event.keyCode == 38){
+        if(yVelocity == 1)
+            return;
       yVelocity = -1;
       xVelocity = 0; 
     }
     //dpwn arrow
     if(event.keyCode == 40){
+        if(yVelocity == -1)
+        return;
         yVelocity = 1;
         xVelocity = 0; 
       }
       //left arrow
       if(event.keyCode == 37){
+        if(xVelocity == 1)
+        return;
         yVelocity = 0;
         xVelocity = -1; 
       }
       // right arrow
       if(event.keyCode == 39){
+        if(xVelocity == -1)
+        return;
         yVelocity = 0;
         xVelocity = 1; 
       }
